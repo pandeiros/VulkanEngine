e330c5d5973a3b0f7de8b6d6f5dc45d1a22f5106
pandeiros <milordpanda@gmail.com>
2018-01-19 13:29:32 +0100

    Added Vulkan build batch file.

---
+++ build/BuildVulkanSDKProjects.bat
@@ -0,0 +1,20 @@
+echo off
+SETLOCAL ENABLEEXTENSIONS
+
+call :build glslang
+call :build spirv-tools
+call :build Samples
+call :build shaderc
+echo Done.
+goto EOF
+
+:build
+cd %VULKAN_SDK%\%1
+echo Building %1
+mkdir build
+cd build
+cmake -G "Visual Studio 15 2017 Win64" ..
+msbuild ALL_BUILD.vcxproj /p:Platform=x64 /p:Configuration=Debug /verbosity:quiet
+msbuild ALL_BUILD.vcxproj /p:Platform=x64 /p:Configuration=Release /verbosity:quiet
+cd ..
+exit /b
